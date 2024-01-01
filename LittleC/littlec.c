8f5d696cabc1e02539fec8e9e26440245b3c9e02
Paco Secades Cuesta <pacosecadescuesta@gmail.com>
2024-01-01 17:39:37 +0000

    Update littlec.c

--- LittleC/littlec.c
+++ LittleC/littlec.c
@@ -1,6 +1,20 @@
 /* A Little C interpreter. 
 
    Minor fixes incorporated as of 1/4/96.
+
+   Herbert Schildt's Little C Interpreter Modified by paco secades
+
+   Adds support for C++ comments
+
+   Add support for repeat until loop structure
+
+   Add suport for &&, || and !
+
+   Add support for break and continue in loop structures
+
+   Please enhanced it and share it!
+
+   enjoy!
    
 */
 
